1655c47f84506cba732cfb7da23ef63bdaaab545
Nguyen Dang Quy <dangquyitt@gmail.com>
2025-02-09 16:36:36 +0700

    Creating and combining views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nguyen Dang Quy on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
